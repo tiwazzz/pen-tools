771117ac485eb8dfead250eef614e87a67af9c48
Tiwat <136233225+tiwazzz@users.noreply.github.com>
2023-08-08 11:56:23 +0700

    Remove functions not used

--- functions.py
+++ functions.py
@@ -1,5 +1,3 @@
-import psutil
-
 def byteToGb(getByte):
     covertedGb = int(getByte)
     covertedGb = round(covertedGb*(10**-9), 2) 
@@ -9,19 +7,3 @@ def byteToMb(getByte):
     covertedMb = int(getByte)
     covertedMb = round(covertedMb*(10**-8), 2)
     return covertedMb
-
-def getRamTotal():
-    try:
-        ramTotal = byteToGb(psutil.virtual_memory().total)
-    except Exception as error:
-        ramTotal = "**:octagonal_sign: libary not support**"
-        print(f"Error Message: {error}")
-    return ramTotal
-
-def getRamSwap():
-    try:
-        swapRam = byteToGb(psutil.swap_memory().total)
-    except Exception as error:
-        swapRam = byteToGb(psutil.swap_memory().total)
-        print(f"Error Message: {error}")
-    return swapRam
